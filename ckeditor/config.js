8d52f266e73fd28aa105dab1c61ba0cf3a9b918d
Habibur Rahaman <shamolrahaman@gmail.com>
2025-09-06 16:54:25 +0600

    Configure MathJax library

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -9,7 +9,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	
 	
-	// config.mathJaxLib = 'https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image';
+        // Path to the MathJax library that enables TeX preview and output
+        config.mathJaxLib = 'https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_HTML';
 
 	 config.extraPlugins = 'mathjax,ckeditor_wiris';
 	   config.removePlugins = 'image,about,iframe,specialchar,emoticons,flash,smiley,pagebreak,preview,print,horizontalrule,stickies,blockselection,div,save,templates,showblocks,scayt,wsc,menubutton,contextmenu,find,selectall,copyformatting,removeformat';
